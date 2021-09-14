9232a990136301b46d70134ff12ff331ffaad214
DucHai-cod <duchai4320@gmail.com>
2021-09-14 19:23:06 +0700

    load image from firebase storage

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import { View, Text, } from 'react-native'
+import React,{useState,useEffect} from 'react'
+import { View, Text, Image, FlatList} from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import storage from '@react-native-firebase/storage';
 
 const Home = () => {
+    const [imageUrl, setImageUrl] = useState([]);
+    const image = ['h1.jpg','h5.jpg']
+    useEffect(async () => {
+        const x= [];
+        for (i in image) {
+            try {
+                const url = await storage().ref('images/' + image[i]).getDownloadURL()
+                x.push({id: image[i], url: url})
+            }
+            catch (err) {
+                console.log('ERROR')
+            }
+        }
+        setImageUrl(x);
+    }, []);
     return (
-        <View>
-            <Text>This is Home</Text>
-        </View>
-    )
-}
+        <FlatList
+        data={imageUrl}
+        renderItem={({item}) => <Image style={{height: 200, width: 200}} source={{uri: item.url}} />}
+      />
+
+        
+    );
+}   
 
 export default Home
