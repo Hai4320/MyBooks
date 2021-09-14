3c81d0f3e43bb056b87c55cb59c1aad944a39404
DucHai-cod <duchai4320@gmail.com>
2021-09-14 12:06:37 +0700

    splash

--- App.js
+++ App.js
@@ -2,11 +2,28 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/navigation/tabs'
+import Splash from './src/screens/Splash'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tabs/>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen
+          name="splash"
+          component={Splash}
+        />
+        <Stack.Screen
+          name="TabBar"
+          component={Tabs}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
---
+++ src/screens/Splash.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import {COLORS} from '../constants'
+
+const Splash = ({navigation}) => {
+    setTimeout(() => {
+        navigation.navigate('TabBar')
+    },1000)
+    return (
+        <View style={styles.container}>
+            <Text style={styles.splash}>My Book</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+     splash: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent:'center',
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: COLORS.tomato,
+        
+     }
+})
+
+
+export default Splash
+
